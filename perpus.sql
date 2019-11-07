99b26c6957db2a046615773ea7135e0a32e4b0c5
albert <albertmaarpi20@yahoo.com>
2019-11-07 09:46:12 +0700

    Update perpus.sql

--- perpus.sql
+++ perpus.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v12.5.1 (64 bit)
-MySQL - 10.4.8-MariaDB : Database - perpustakaan
+MySQL - 10.4.6-MariaDB : Database - perpustakaan
 *********************************************************************
 */
 
@@ -12,7 +12,7 @@ MySQL - 10.4.8-MariaDB : Database - perpustakaan
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`perpustakaan` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`perpustakaan` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `perpustakaan`;
 
@@ -90,6 +90,22 @@ CREATE TABLE `cd` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `cd` */
+
+/*Table structure for table `userperpus` */
+
+DROP TABLE IF EXISTS `userperpus`;
+
+CREATE TABLE `userperpus` (
+  `id_user` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `jabatan` int(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `userperpus` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
