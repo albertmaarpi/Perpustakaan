0bc3522d4d77ddc6e770ca96aa17c3302ca13d50
albert <albertmaarpi20@yahoo.com>
2019-11-21 09:27:32 +0700

    insert cd

--- Perpustakaan/datacd.cs
+++ Perpustakaan/datacd.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace Perpustakaan
 {
     public partial class datacd : Form
     {
+        public MySqlDataAdapter da;
+        connection conn;
+        DataSet ds;
         public datacd()
         {
             InitializeComponent();
@@ -48,6 +51,37 @@ namespace Perpustakaan
                 fc.ShowDialog();
 
             }
+        }
+
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn.myconnection();
+                cmd.CommandText = "insert into cd" +
+                    "(judul,jenis,harga,keterangan,kode,kondisi,rak)" +
+                    "values(@judul,@jenis,@harga,@keterangan,@kode,@kondisi,@rak)";
+                cmd.Parameters.Add(new MySqlParameter("@judul", txtjudul.Text));
+                cmd.Parameters.Add(new MySqlParameter("@jenis", cbjenis.Text));
+                cmd.Parameters.Add(new MySqlParameter("@harga", txtharga.Text));
+                cmd.Parameters.Add(new MySqlParameter("@keterangan", rbketerangan.Text));
+                cmd.Parameters.Add(new MySqlParameter("@kode", txtkode.Text));
+                cmd.Parameters.Add(new MySqlParameter("@kondisi", cbkondisi.Text));
+                cmd.Parameters.Add(new MySqlParameter("@rak", txtrak.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("insert successful");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void datacd_Load(object sender, EventArgs e)
+        {
+            conn = new connection();
         }
     }
 }
